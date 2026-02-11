7755bc2f5e7881d5fc6dc2f75cd43323d140060b
dB-G3 <gotoyasuhiro@gmail.com>
2026-02-11 23:36:04 +0900

    動画サムネイル抽出、動画圧縮の追加

--- local_processor/src/main.py
+++ local_processor/src/main.py
@@ -7,8 +7,14 @@ from PIL import ImageOps
 import datetime
 import hashlib
 
+#動画用
+import cv2
+import subprocess
+
 TARGET_WIDTH = 800
 TARGET_HEIGHT = 600
+VIDEOHEIGHT = "600"
+VIDEOQUALITY = "28"  # 画質設定（18〜28が一般的。数字が大きいほど高圧縮・低画質）
 BASE_DIR = "../"
 INPUT_DIR = os.path.join(BASE_DIR, "test-data/input")
 OUTPUT_DIR = os.path.join(BASE_DIR, "test-data/output")
@@ -71,6 +77,51 @@ def calculate_hash(filepath):
             sha256_hash.update(byte_block)
     return sha256_hash.hexdigest()
 
+def extract_video_thumbnail(video_file, save_path, second=1.0):
+    # 動画ファイルを開く
+    cap = cv2.VideoCapture(video_file)
+    
+    if not cap.isOpened():
+        print(f"  [動画オープン失敗] {relative_path}")
+        with open(OUTPUT_DIR + '/' + LOGFILE_NAME,"a") as o:
+            print(relative_path + "動画オープン失敗", file=o)
+        return
+
+    # 動画のFPS（フレームレート）を取得
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    # 指定した秒数のフレーム位置を計算
+    frame_id = int(fps * second)
+    
+    # 指定フレームまで移動
+    cap.set(cv2.CAP_PROP_POS_FRAMES, frame_id)
+    
+    # フレームを読み込む
+    ret, frame = cap.read()
+    if ret:
+        # 画像として保存（リサイズも可能）)
+        cv2.imwrite(save_path, frame)
+        print(f"サムネイルを保存しました: {save_path}")
+    else:
+        print(f"  [フレーム読み込み失敗] {relative_path}")
+        with open(OUTPUT_DIR + '/' + LOGFILE_NAME,"a") as o:
+            print(relative_path + "フレーム読み込み失敗", file=o)
+    
+    cap.release()
+
+def compress_video(input_path, output_path):
+    cmd = [
+        'ffmpeg', '-i', input_path,
+        '-vcodec', 'libx264',  # 汎用性の高いH.264ビデオコーデック
+        '-crf', VIDEOQUALITY,
+        '-preset', 'medium',   # 圧縮スピード（slowにすると時間はかかるがより綺麗に圧縮される）
+        '-acodec', 'aac',      # 音声も標準的な形式に変換
+        '-vf', "scale=-2:"+VIDEOHEIGHT, # 高さ720ピクセルにリサイズ（幅はアスペクト比を維持）
+        output_path,
+        '-y'
+    ]
+    subprocess.run(cmd)
+    print(f"動画を圧縮しました: {output_path}")
+
 if __name__ == "__main__":
     print(f"--- 探索開始: {INPUT_DIR} ---")
     
@@ -113,5 +164,9 @@ if __name__ == "__main__":
                 with open(OUTPUT_DIR + '/' + LOGFILE_NAME,"a") as o:
                     log_data = log_data + hash + ","
                     print(log_data, file=o)
+            elif img_file.suffix.lower() in [".mov"]:
+                extract_video_thumbnail(img_file, save_path.with_suffix(".jpg"), 1.0)
+                compress_video(img_file, save_path)
+            
             else:
                 print(relative_path)
