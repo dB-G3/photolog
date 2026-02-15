2e240ce4b66aca2c861fc2a794d198f0d28907dc
dB-G3 <gotoyasuhiro@gmail.com>
2026-02-15 23:42:54 +0900

    撮影日毎フォルダへ保存

--- local_processor/src/main.py
+++ local_processor/src/main.py
@@ -27,12 +27,6 @@ if __name__ == "__main__":
             # 入力フォルダからの相対パスを取得 (例: 2024/01/pic.jpg)
             relative_path = img_file.relative_to(input_path)
             log_data = log_data + str(relative_path) + ","
-    
-            # 保存先のフルパスを決定
-            save_path = Path(OUTPUT_DIR) / relative_path
-    
-            # 保存先のフォルダがなければ作成 (mkdir -p 相当)
-            save_path.parent.mkdir(parents=True, exist_ok=True)
 
             iso_date = "撮影日時取得不可"
             if img_file.suffix.lower() in [".jpg", ".jpeg", ".heic"]:
@@ -43,23 +37,50 @@ if __name__ == "__main__":
                     dt = datetime.datetime.strptime(exif_data.get('DateTime'), "%Y:%m:%d %H:%M:%S")
                 # ISO 8601形式の文字列に変換
                 iso_date = dt.isoformat()
+
+                # 保存先のフルパスを決定
+                # datetimeオブジェクトに変換
+                try:
+                    dt = datetime.datetime.fromisoformat(iso_date)
+                    # 日付だけの文字列にする (2025-10-20)
+                    dir_name = dt.date().isoformat() 
+                except Exception as e:
+                    print(e)
+                    dir_name = "1970-01-01"
+                # 保存先フォルダなければ作成
+                temp_dir = Path(OUTPUT_DIR) / dir_name
+                temp_dir.mkdir(parents=True, exist_ok=True)
+                save_path_pict = temp_dir / relative_path.name
                 
-                img = picture.process_image(img_file, TARGET_WIDTH, TARGET_HEIGHT, save_path, relative_path, OUTPUT_DIR)
+                img = picture.process_image(img_file, TARGET_WIDTH, TARGET_HEIGHT, save_path_pict, relative_path, OUTPUT_DIR)
 
             elif img_file.suffix.lower() in [".mov"]:
                 meta = movie.get_video_metadata(img_file)
                 if(meta['Creation date']):  #撮影日時
                     dt = datetime.datetime.strptime(meta['Creation date'], "%Y-%m-%d %H:%M:%S")
-                    iso_date = dt.isoformat()
                 elif(meta['Last modification']):    #修正日時
                     dt = datetime.datetime.strptime(meta['Last modification'], "%Y-%m-%d %H:%M:%S")
-                    iso_date = dt.isoformat()
                 # ISO 8601形式の文字列に変換
                 iso_date = dt.isoformat()
+
+                # 保存先のフルパスを決定
+                try:
+                    dt = datetime.datetime.fromisoformat(iso_date)
+                    # 日付だけの文字列にする (2025-10-20)
+                    dir_name = dt.date().isoformat() 
+                except Exception as e:
+                    print(e)
+                    dir_name = "1970-01-01"
+                # 保存先フォルダなければ作成
+                temp_dir = Path(OUTPUT_DIR) / dir_name
+                temp_dir.mkdir(parents=True, exist_ok=True)
+                save_path_video = temp_dir / relative_path.name
+                save_path_video_thumbnail = temp_dir / relative_path.name
                 
                 print(f"動画処理開始: {relative_path}")
-                movie.extract_video_thumbnail(img_file, save_path.with_suffix(".jpg"), relative_path, 1.0)
-                movie.compress_video(img_file, save_path)
+                #print(save_path_video)
+                movie.extract_video_thumbnail(img_file, save_path_video_thumbnail.with_suffix(".jpg"), relative_path, 1.0)
+                movie.compress_video(img_file, save_path_video)
             
             else:
                 print("処理スキップ：" + str(relative_path))
