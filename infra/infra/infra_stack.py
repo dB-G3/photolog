803dd1390c4dd8ad9505710d304c94ed1e51db1c
dB-G3 <gotoyasuhiro@gmail.com>
2026-02-28 20:50:24 +0900

    dynamoDBと二つ目のS3バケット追加

--- infra/infra/infra_stack.py
+++ infra/infra/infra_stack.py
@@ -2,6 +2,7 @@ from aws_cdk import (
     # Duration,
     Stack,
     aws_s3 as s3,
+    aws_dynamodb as dynamodb,
     RemovalPolicy,
     # aws_sqs as sqs,
 )
@@ -22,12 +23,35 @@ class InfraStack(Stack):
         # )
 
         # 写真を保存するS3バケットの定義
-        owner = os.getenv("OWNER_NAME_01", "default_user")
-        bucket = s3.Bucket(self, "PhotologRawBucket",
-            # バケット名を世界で一意にするため、自動生成に任せるか指定します
-            # 指定する場合: bucket_name="my-unique-photolog-bucket-2024",
+        bucket_yasu = s3.Bucket(self, "PhotologRawBucket-yasu-v2",
             versioned=True,  # 誤削除防止のためにバージョン管理を有効化
             removal_policy=RemovalPolicy.DESTROY, # 学習用なのでStack削除時にバケットも消す設定（本番はRETAIN推奨）
             auto_delete_objects=True, # バケット削除時に中身も自動削除する
-            bucket_name=f"photolog-prod-s3-original-{owner}"
+            bucket_name=f"photolog-prod-s3-original-yasu"
+        )
+
+        bucket_megu = s3.Bucket(self, "PhotologRawBucket-megu-v2",
+            versioned=True,  # 誤削除防止のためにバージョン管理を有効化
+            removal_policy=RemovalPolicy.DESTROY, # 学習用なのでStack削除時にバケットも消す設定（本番はRETAIN推奨）
+            auto_delete_objects=True, # バケット削除時に中身も自動削除する
+            bucket_name=f"photolog-prod-s3-original-megu"
+        )
+
+        # DynamoDBの定義
+        table = dynamodb.Table(
+            self, "PhotologMetadataTable",
+            table_name=f"photolog-prod-dynamodb-metadata-yasu",
+            # パーティションキー: ユーザーを一意に識別するID (例: megu)
+            partition_key=dynamodb.Attribute(
+                name="UserID", 
+                type=dynamodb.AttributeType.STRING
+            ),
+            # ソートキー: 撮影日（日付で検索・ソートしやすくするため）
+            sort_key=dynamodb.Attribute(
+                name="ShootingDate", 
+                type=dynamodb.AttributeType.STRING
+            ),
+            # 開発用設定
+            billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST, # 使った分だけ課金
+            removal_policy=RemovalPolicy.DESTROY             # Stack削除時にテーブルも消す
         )
