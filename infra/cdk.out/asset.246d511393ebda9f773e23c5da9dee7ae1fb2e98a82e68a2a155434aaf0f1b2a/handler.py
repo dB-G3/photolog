eede599f1e0746d8caf5f851c1851559f7d6a03c
dB-G3 <gotoyasuhiro@gmail.com>
2026-03-15 13:21:31 +0900

    chore: remove cdk.out from repository history

--- infra/cdk.out/asset.246d511393ebda9f773e23c5da9dee7ae1fb2e98a82e68a2a155434aaf0f1b2a/handler.py
+++
@@ -1,7 +0,0 @@
-# /workspaces/photolog/lambda_S3meta/handler.py
-def lambda_handler(event, context):
-    print("Event: ", event)
-    return {
-        'statusCode': 200,
-        'body': 'Hello from S3 Event Lambda!'
-    }
